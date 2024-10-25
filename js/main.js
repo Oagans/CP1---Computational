f3ff102d5a856e7f53abe558a04de8b3671b30fb
Matheus <matnagao@gmail.com>
2024-10-25 14:15:07 -0300

    Adição do link do jsonplaceholder

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 const botao = document.getElementById('meuBotao');
 const resultado = document.getElementById('resultado');
+
+
+funcition (buscarDados)() {
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    
+}
